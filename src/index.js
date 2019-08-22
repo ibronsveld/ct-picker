774750ebdcd8a02c5a9873df1c06973b3fa94225
Ivo Bronsveld <ivo.bronsveld@commercetools.com>
2019-08-22 13:37:29 +0200

    Made URL absolute

--- src/index.js
+++ src/index.js
@@ -72,8 +72,8 @@ class CTPicker {
     if (self.containerElement) {
       // Load the template from the URL
       // TODO: How do we handle this in production?
-      let url = "picker.html";
-      // let url = "https://ct-merchant-test.herokuapp.com/src/picker.html";
+      //let url = "picker.html";
+      let url = "https://ct-merchant-test.herokuapp.com/src/picker.html";
       fetch(url).then((response) => {
         response.text().then((modalUIData) => {
           // Adds the HTML scripts to the page for use with Handlebars
